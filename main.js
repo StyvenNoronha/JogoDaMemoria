9f449d7cb4822f5f92c73bf5d426d9d15cc51320
Styven Noronha <styvenn16@gmail.com>
2023-01-31 07:07:43 -0300

    primeiro

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+import './src/styles/elements/base.css'
+import './src/styles/settings/colors.css'
+import './src/styles/generic/reset.css'
+import CardGame from './src/components/CardGame'
+
+
+
+import PlayerScore from './src/components/PlayerScore'
+const root = document.querySelector("#root")
+
+const $htmlCardGame = CardGame();
+
+console.log($htmlCardGame);
+root.insertAdjacentHTML('beforeend',$htmlCardGame)
---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+function CardGame() {
+  return `
+  <div class="card-game">
+   <img src="./images/Lobo.png" alt="lobo em tribal">
+  </div>
+  `
+}
+
+export default CardGame;
