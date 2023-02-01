092e81d325f306c6e0624c2c9d42f4586a89f416
Styven Noronha <styvenn16@gmail.com>
2023-02-01 07:06:25 -0300

    criacao do board game

--- main.js
+++ main.js
@@ -3,14 +3,11 @@
 import './src/styles/elements/base.css'
 import './src/styles/settings/colors.css'
 import './src/styles/generic/reset.css'
-import CardGame from './src/components/CardGame'
 
 
-
-import PlayerScore from './src/components/PlayerScore'
+import BoardGame from './src/objects/BoardGame'
 const root = document.querySelector("#root")
+const htmlBoardGame = BoardGame(3)
 
-const $htmlCardGame = CardGame();
 
-console.log($htmlCardGame);
-root.insertAdjacentHTML('beforeend',$htmlCardGame)
+root.insertAdjacentHTML("beforeend", htmlBoardGame)
