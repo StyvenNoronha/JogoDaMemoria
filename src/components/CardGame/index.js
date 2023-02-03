02b15d32392db3773d0d383bda0182c7b05e2da3
Styven Noronha <styvenn16@gmail.com>
2023-02-03 19:37:38 -0300

    correcao imagem

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -3,7 +3,7 @@ import './style.css'
 function CardGame() {
   return `
   <div class="card-game">
-   <img src="./images/Lobo.png" alt="lobo em tribal">
+   <img src="../../../images/Lobo.png" alt="lobo em tribal">
   </div>
   `
 }
